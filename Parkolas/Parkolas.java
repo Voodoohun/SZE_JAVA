e8406d5edbe8abe3eadec05737ed981743099cb8
boszedave <34679750+Voodoohun@users.noreply.github.com>
2020-01-12 22:30:23 +0100

    nemmukodik

---
+++ Parkolas/Parkolas.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Dave
+ */
+
+import java.time.*;
+
+public class Parkolas {
+    
+    String rendszam;
+    String pHely;
+    LocalDateTime parkIdopont;
+
+    public Parkolas(String rendszam, String pHely, LocalDateTime parkIdopont) {
+        this.rendszam = rendszam;
+        this.pHely = pHely;
+        this.parkIdopont = parkIdopont;
+    }
+    
+    
+}
